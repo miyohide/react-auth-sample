817f1105d317b4f156c3cb3f1f64be3263e29f8b
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-24 20:33:27 +0900

    create new App.js

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css';
+import Dashboard from "../Dashboard/Dashboard";
+import Preferences from "../Preferences/Preferences";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return(
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
