62b1af9971309ac103152f2c67cbd2be5d20eb4b
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-25 18:48:32 +0900

    run format

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,26 +3,26 @@ import './Login.css';
 import PropTypes from "prop-types";
 
 export default function Login({setToken}) {
-  return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
+    return (
+        <div className="login-wrapper">
+            <h1>Please Log In</h1>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text"/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password"/>
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  )
+    )
 }
 
 Login.propTypes = {
-  setToken: PropTypes.func.isRequired
+    setToken: PropTypes.func.isRequired
 }
